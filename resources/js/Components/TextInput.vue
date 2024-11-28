a63799fe9067e91b52899d255948bd05a1342527
haikaljs <haikaljavascript@gmail.com>
2024-11-28 21:32:30 +0800

    SearchForm component

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,5 +1,13 @@
+<template>
+    <input
+        class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        v-model="model"
+        ref="input"
+    />
+</template>
+
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from "vue";
 
 const model = defineModel({
     type: String,
@@ -9,18 +17,10 @@ const model = defineModel({
 const input = ref(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
+    if (input.value.hasAttribute("autofocus")) {
         input.value.focus();
     }
 });
 
 defineExpose({ focus: () => input.value.focus() });
 </script>
-
-<template>
-    <input
-        class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-        v-model="model"
-        ref="input"
-    />
-</template>
--- resources/js/Components/app/SearchForm.vue
+++ resources/js/Components/app/SearchForm.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>Search Form</div>
+    <form action="" class="w-[600px] h-[80px] flex items-center">
+        <TextInput
+            type="text"
+            class="block w-full mr-2"
+            v-model="form.search"
+            autocomplete
+            placeholder="Search for files and folders"
+        />
+    </form>
 </template>
 
 <script setup>
 // Imports
+import TextInput from "@/Components/TextInput.vue";
+import { useForm } from "@inertiajs/vue3";
 
 // Uses
+const form = useForm({
+    search: ''
+})
 
 // Refs
 
